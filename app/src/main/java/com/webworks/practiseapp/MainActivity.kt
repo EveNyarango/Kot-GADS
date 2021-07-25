e1af009a2728663581327a79662c1d4e127934da
EveNyarango <evenyarango@gmail.com>
2021-07-25 12:04:55 +0300

    altered UI

--- app/src/main/java/com/webworks/practiseapp/MainActivity.kt
+++ app/src/main/java/com/webworks/practiseapp/MainActivity.kt
@@ -4,12 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatEditText
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-
+companion object{
+    private val TAG: String = MainActivity::class.java.simpleName
+}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,25 +27,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun listRepositories(view: View) {
-        val queryRepo = etRepoName.text.toString()
-        val  repoLanguage = etLanguage.text.toString()
-        val intent = Intent(this, DisplayActivity::class.java)
-        intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_REPO)
-        intent.putExtra(Constants.KEY_REPO_SEARCH, queryRepo)
-        intent.putExtra(Constants.KEY_LANGUAGE,repoLanguage)
-        startActivity(intent)
-
+        if(isNotEmpty(etRepoName, inputLayoutRepoName)) {
+            val queryRepo = etRepoName.text.toString()
+            val repoLanguage = etLanguage.text.toString()
+            val intent = Intent(this, DisplayActivity::class.java)
+            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_REPO)
+            intent.putExtra(Constants.KEY_REPO_SEARCH, queryRepo)
+            intent.putExtra(Constants.KEY_LANGUAGE, repoLanguage)
+            startActivity(intent)
+        }
     }
 
     fun listUserRepositories(view: View) {
-        val githubUser = etGithubUser.text.toString()
+        if(isNotEmpty(etGithubUser, inputLayoutGithubUser)) {
+            val githubUser = etGithubUser.text.toString()
 
 //        val cls: Class<DisplayActivity> = DisplayActivity::class.java
 
-        val intent = Intent(this, DisplayActivity::class.java)
-        intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_USER)
-        intent.putExtra(Constants.KEY_GITHUB_USER, githubUser)
-        startActivity(intent)
+            val intent = Intent(this, DisplayActivity::class.java)
+            intent.putExtra(Constants.KEY_QUERY_TYPE, Constants.SEARCH_BY_USER)
+            intent.putExtra(Constants.KEY_GITHUB_USER, githubUser)
+            startActivity(intent)
 
+        }
+
+    }
+
+    fun isNotEmpty(editText: EditText, textInputLayout: TextInputLayout) : Boolean{
+        if (editText.text.toString().isEmpty()){
+            textInputLayout.error = "Cannot be blank !"
+            return false
+        }else{
+            textInputLayout.isErrorEnabled = false
+            return true
+        }
     }
 }
